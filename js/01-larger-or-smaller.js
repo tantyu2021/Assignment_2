fa3442fe9b0970055f7d61eaffe3237ee989fdde
tantyu2021 <tantyu2021@gmail.com>
2020-02-25 12:25:52 -0800

    add some in js01 larger number

--- js/01-larger-or-smaller.js
+++ js/01-larger-or-smaller.js
@@ -1,6 +1,44 @@
  // two integers within two separate prompts. Display only the large of two   
         //  more compact syntax
-  
+console.log("compact syntex");        
         let houses = parseFloat(window.prompt("Enter how many houses on this street"));
         let trees = parseFloat(window.prompt("Enter number of trees"));
-           (trees >= houses) ? window.alert("trees") : window.alert("houses");      
+           (trees >= houses) ? window.alert("trees") : window.alert("houses"); 
+ 
+console.log("staff - client");
+           function greaterNum(staff, client) {
+            if (staff === client) {
+                console.log ("Those numbers are equel");
+                return staff;
+            } else if (staff > client) {
+                return staff;
+            } else if (client > staff) {
+                return client;
+            } else {
+                console.log ("Those are incomparable!");
+                return undefined;
+            }
+        }
+        console.log(greaterNum(4, 4));
+        console.log(greaterNum(8, 2));
+        console.log(greaterNum(3, 13));
+        console.log(greaterNum(3, 'cat'));
+
+console.log("large number");
+function largeNum(num1, num2) {
+   if (num1 === num2) {
+       console.log ("Those numbers are equel");
+       return num1;
+   } else if (num1 > num2) {
+       return num1;
+   } else if (num2 > num1) {
+       return num2;
+   } else {
+       console.log ("Those are incomparable!");
+       return undefined;
+   }
+}
+        console.log(largeNum(3, 3));
+        console.log(largeNum(8, -3));
+        console.log(largeNum(3, 13));
+        console.log(largeNum(3, 'cat'));
