ba2edb9ec869945b2f425ccd29af255cbee9a11d
tantyu2021 <tantyu2021@gmail.com>
2020-02-25 12:35:38 -0800

    add change to js08 countdown

--- js/08-countdown.js
+++ js/08-countdown.js
@@ -1,4 +1,21 @@
-// counts down 100 times
-for (var x = 100; x < 100; x--;) {
-    window.alert(x);
+// counts down  
+//console.log("count down");
+//window.document.write("count down from 10 ");
+let a;
+for (a = 10; a >= 0; a -= 1) {
+window.document.write(a + "<br>");
+}        
+        
+ 
+ //console.log("count down");
+//window.document.write("count down from 20 to 0");
+let b;
+for (b = 20; b >= 0; b -= 1) {
+window.document.write(b + "<br>");
+}          
+        
+console.log("count up");
+let i;
+for (i = 1; i <= 10; i += 1) {
+window.document.write(i + "<br>");
 }
